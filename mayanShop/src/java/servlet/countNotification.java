99d543e1913b5a36837f39df0321a95a77cec8fc
Bat95 <thomas.batzella@gmail.com>
2018-01-24 16:23:43 +0100

    Aggiunta parziale notifiche pt2

--- mayanShop/src/java/servlet/countNotification.java
+++ mayanShop/src/java/servlet/countNotification.java
@@ -38,7 +38,6 @@ public class countNotification extends HttpServlet {
         
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
