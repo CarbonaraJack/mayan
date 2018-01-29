18ef88dd4a7f67a0ffb915db79e45fa495c62eef
CarbonaraJack <linked.uno@gmail.com>
2018-01-29 21:35:07 +0100

    Merge con il master e abbandono vecchio branch per problemi con messageBean

--- mayanShop/src/java/dbLayer/gestioneSegnalazione.java
+++ mayanShop/src/java/dbLayer/gestioneSegnalazione.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dbLayer;
 
 import bean.messaggioBean;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -18,64 +12,65 @@ import java.util.logging.Logger;
  * @author Thomas
  */
 public class gestioneSegnalazione {
- 
-    public static boolean risSegnalazione(String text, int idM, int idD, int idT, int idR){
-        boolean isDone=false;
+
+    public static boolean risSegnalazione(String text, int idM, int idD, int idT, int idR) {
+        boolean isDone = false;
         Connection connection = DAOFactoryUsers.getConnection();
-        
-        String query = "INSERT INTO Messaggio (tipo, descrizione, stato, id_risposta, id_destinatario, id_mittente, id_transazione, letto)" +
-                    "VALUES ('risposta', '" + text + "', 'aperta', '"+ idR +"', '" + idD + "', '" + idM + "', '" + idT+ "', '0');";
+
+        String query = "INSERT INTO Messaggio (tipo, descrizione, stato, id_risposta, id_destinatario, id_mittente, id_transazione, letto)"
+                + "VALUES ('risposta', '" + text + "', 'aperta', '" + idR + "', '" + idD + "', '" + idM + "', '" + idT + "', '0');";
         try {
             Statement st = connection.createStatement();
             int i = st.executeUpdate(query);
-            
-            if(i==1){
-                isDone = true;                       
+
+            if (i == 1) {
+                isDone = true;
             }
-            
+            connection.close();
         } catch (SQLException ex) {
             Logger.getLogger(gestioneSegnalazione.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return isDone;
-    } 
-    
-    public static boolean rifSegnalazione(String text, int idM, int idD, int idT, int idR){
-        boolean isDone=false;
+    }
+
+    public static boolean rifSegnalazione(String text, int idM, int idD, int idT, int idR) {
+        boolean isDone = false;
         Connection connection = DAOFactoryUsers.getConnection();
-        
-        String query = "INSERT INTO Messaggio (tipo, descrizione, stato, id_risposta, id_destinatario, id_mittente, id_transazione, letto)" +
-                    "VALUES ('risoluzione', '" + text + "', 'chiusa', '"+ idR +"', '" + idD + "', '" + idM + "', '" + idT+ "', '0');";
+
+        String query = "INSERT INTO Messaggio (tipo, descrizione, stato, id_risposta, id_destinatario, id_mittente, id_transazione, letto)"
+                + "VALUES ('risoluzione', '" + text + "', 'chiusa', '" + idR + "', '" + idD + "', '" + idM + "', '" + idT + "', '0');";
         try {
             Statement st = connection.createStatement();
-            int i = st.executeUpdate(query);            
-            if(i==1){
-                isDone = true;                       
+            int i = st.executeUpdate(query);
+            if (i == 1) {
+                isDone = true;
             }
-            
+            connection.close();
         } catch (SQLException ex) {
             Logger.getLogger(gestioneSegnalazione.class.getName()).log(Level.SEVERE, null, ex);
         }
         isDone = setChiusa(idR);
         return isDone;
-    } 
-    
-    public static boolean setChiusa(int idM){
+    }
+
+    public static boolean setChiusa(int idM) {
         boolean isDone = false;
         Connection connection = DAOFactoryUsers.getConnection();
         messaggioBean m = dbLayer.messaggioDAO.getMessage(idM);
-        while(m.getIdRisposta() != -1){
-            String query = "UPDATE SET stato='chiusa' WHERE id_risposta='"+idM+"';";
+        while (m.getIdRisposta() != -1) {
+            String query = "UPDATE SET stato='chiusa' WHERE id_risposta='" + idM + "';";
             try {
                 Statement st = connection.createStatement();
-                int i= st.executeUpdate(query);
-                if (i==1){
+                int i = st.executeUpdate(query);
+                if (i == 1) {
                     isDone = true;
                 }
+                connection.close();
             } catch (SQLException ex) {
                 Logger.getLogger(gestioneSegnalazione.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
             isDone = setChiusa(m.getIdRisposta());
         }
         return isDone;
--- mayanShop/src/java/dbLayer/messaggioDAO.java
+++ mayanShop/src/java/dbLayer/messaggioDAO.java
@@ -71,91 +71,91 @@ public class messaggioDAO {
         }
         return res;
     }
-    
-    public static messaggioBean getMessage(int idM){
+
+    public static messaggioBean getMessage(int idM) {
         Connection connection = DAOFactoryUsers.getConnection();
         messaggioBean m = new messaggioBean();
-        String query = "SELECT * FROM Messaggio WHERE id_messaggio='" + idM + "';";        
-        try {
-            Statement st =  connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            if(rs.next()){
-                return new messaggioBean(
-                        
-                        rs.getInt("id_messaggio"),
-                        rs.getString("tipo"), 
-                        rs.getString("descrizione"), 
-                        rs.getString("stato"), 
-                        rs.getInt("id_risposta"),  
-                        rs.getInt("id_destinatario"), 
-                        rs.getInt("id_mittente"), 
-                        rs.getInt("id_transazione"), 
-                        rs.getInt("letto"), 
-                        rs.getString(findUserInf(rs.getString("id_destinatario")).get(0)),
-                        rs.getString(findUserInf(rs.getString("id_mittente")).get(0)));
-            }
-        } catch (SQLException ex) { 
-            Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
-
-        
-    public static ArrayList<String> findUserInf(String id){
-        Connection connection = DAOFactoryUsers.getConnection();
-        ArrayList<String> res = new ArrayList<>();
-        String query="SELECT nome,cognome,tipo FROM User WHERE id_user='"+ id +"';";
+        String query = "SELECT * FROM Messaggio WHERE id_messaggio='" + idM + "';";
         try {
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(query);
-            if (rs.next()){
+            if (rs.next()) {
+                m = new messaggioBean(
+                        rs.getInt("id_messaggio"),
+                        rs.getString("tipo"),
+                        rs.getString("descrizione"),
+                        rs.getString("stato"),
+                        rs.getInt("id_risposta"),
+                        rs.getInt("id_destinatario"),
+                        rs.getInt("id_mittente"),
+                        rs.getInt("id_transazione"),
+                        rs.getInt("letto"),
+                        rs.getString(findUserInf(rs.getString("id_destinatario")).get(0)),
+                        rs.getString(findUserInf(rs.getString("id_mittente")).get(0)));
+            }
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return m;
+    }
+
+    public static ArrayList<String> findUserInf(String id) {
+        Connection connection = DAOFactoryUsers.getConnection();
+        ArrayList<String> res = new ArrayList<>();
+        String query = "SELECT nome,cognome,tipo FROM User WHERE id_user='" + id + "';";
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            if (rs.next()) {
                 res.add(rs.getString("nome") + " " + rs.getString("cognome"));
                 res.add(rs.getString("tipo"));
             }
-        } catch (SQLException ex) { 
+            connection.close();
+        } catch (SQLException ex) {
             Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return res;
     }
-    
-    public static void update(int idMessaggio){
-        
+
+    public static void update(int idMessaggio) {
+
         Connection connection = DAOFactoryUsers.getConnection();
         String query = "UPDATE Messaggio SET letto=1 WHERE id_messaggio='" + idMessaggio + "';";
         try {
             Statement st = connection.createStatement();
             int i = st.executeUpdate(query);
-           
-        } catch (SQLException ex) {       
+            connection.close();
+        } catch (SQLException ex) {
             Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static ArrayList<messaggioBean> getMessaggeList(boolean admin, int userId){
-        
+
+    public static ArrayList<messaggioBean> getMessaggeList(boolean admin, int userId) {
+
         ArrayList<messaggioBean> res = new ArrayList<>();
         Connection connection = DAOFactoryUsers.getConnection();
         ResultSet rs;
-        
+
         try {
-            
+
             //Cerco tutti i messaggi indirizzati a me
             String query = "SELECT id_messaggio, tipo, id_destinatario, id_mittente, id_transazione, letto FROM Messaggio WHERE ";
-            if (admin){
-                query += "id_destinatario!='" + userId + "' AND id_mittente!='"+ userId +"' AND tipo='anomalia' ";
+            if (admin) {
+                query += "id_destinatario!='" + userId + "' AND id_mittente!='" + userId + "' AND tipo='anomalia' ";
             } else {
-                query += "id_destinatario='"+ userId + "' ";
+                query += "id_destinatario='" + userId + "' ";
             }
             query += "ORDER BY id_messaggio;";
-            
+
             Statement st = connection.prepareStatement(query);
-            rs = st.executeQuery(query);            
-            
+            rs = st.executeQuery(query);
+
             boolean isEmpty = !rs.first();
-            if(!isEmpty){
+            if (!isEmpty) {
                 //Creo il codice per ogni elemento
-                while(rs.next()){                    
+                while (rs.next()) {
                     messaggioBean m = new messaggioBean();
                     m.setTipo(rs.getString("tipo"));
                     m.setDescrizione(rs.getString("descrizione"));
@@ -169,37 +169,39 @@ public class messaggioDAO {
                     m.setNomeMittente(rs.getString(findUserInf(rs.getString("id_mittente")).get(0)));
                     res.add(m);
                 }
-            } 
-        } catch (SQLException ex) { 
+            }
+            connection.close();
+        } catch (SQLException ex) {
             Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return res;
     }
-    
-    public static int getUnreadCounter(boolean admin, int idM){
- 
+
+    public static int getUnreadCounter(boolean admin, int idM) {
+
         Connection connection = DAOFactoryUsers.getConnection();
         String query = "SELECT id_messaggio FROM Messaggio WHERE letto='0' AND ";
-        if (admin){
-            query += "id_destinatario!='" + idM + "' AND id_mittente!='"+ idM +"' AND tipo='anomalia'";
+        if (admin) {
+            query += "id_destinatario!='" + idM + "' AND id_mittente!='" + idM + "' AND tipo='anomalia'";
         } else {
-            query += "id_destinatario='"+ idM + "'";
+            query += "id_destinatario='" + idM + "'";
         }
         query += ";";
-        
+
         int count = 0;
-        
+
         try {
             Statement st = connection.prepareStatement(query);
-            ResultSet rs = st.executeQuery(query);        
-            
+            ResultSet rs = st.executeQuery(query);
+
             boolean isEmpty = !rs.first();
-            if(!isEmpty){
-                while(rs.next()){
+            if (!isEmpty) {
+                while (rs.next()) {
                     count++;
                 }
-            }            
-        } catch (SQLException ex) { 
+            }
+            connection.close();
+        } catch (SQLException ex) {
             Logger.getLogger(messaggioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return count;
